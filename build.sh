2f9c0ea25000e379aa7ce66d1c3379ec4fab8251
Rui Zhang <rui.zhang@mongodb.com>
2014-12-31 10:16:31 -0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+function build() {
+    platform=$1
+    GOARCH=amd64 CGO_ENABLE=0 GOPATH=`pwd`:$GOPATH \
+        go build -o ./bin/hammer.$platform ./hammer.mongo.go
+}
+
+## echo "build binary for Linux"
+## GOOS=linux build linux
+
+echo "build binary for MacOS"
+GOOS=darwin build macos
