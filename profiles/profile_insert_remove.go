1492e90724041a26da52bad2ec195b5c451d6a51
Rui Zhang <rui.zhang@mongodb.com>
2015-02-03 10:33:58 -0800

    make insert_remove with write command

--- profiles/profile_insert_remove.go
+++ profiles/profile_insert_remove.go
@@ -23,7 +23,8 @@ var _insertDeleteProfile insertDeleteProfile
 
 func (i insertDeleteProfile) SendNext(s *mgo.Session, worker_id int) error {
 
-	c := s.DB("test1").C("people")
+	// c := s.DB("test1").C("people")
+	c := s.DB(getDBName(default_db_name_prefix)).C(getCollectionName(default_col_name_prefix))
 
 	if _insertDeleteProfile.MaxUID == 0 {
 		// to find out how many records we have
@@ -38,19 +39,23 @@ func (i insertDeleteProfile) SendNext(s *mgo.Session, worker_id int) error {
 
 	_u := rand.Int63n(_insertDeleteProfile.MaxUID) // to find a random person
 
-	c.Remove(bson.M{"_id": _u})
-	c.Insert(bson.M{
-		"_id":      _u,
-		"name":     _u,
-		"group":    rand.Int(),
-		"payload":  &Payload,
-		"payload1": &Payload1,
-		"payload2": &Payload2,
-		"payload3": &Payload3,
-		"payload4": &Payload4,
-		"payload5": &Payload5,
-		"payload6": &Payload6})
+	doc := bson.M{
+		"_id":     _u,
+		"name":    _u,
+		"group":   rand.Int(),
+		"payload": &Payload}
 
+	if _profile_use_legacy_write {
+		c.Remove(bson.M{"_id": _u})
+		c.Insert(doc)
+	} else {
+		var results interface{}
+
+		c.Database.Run(bson.D{{"delete", c.Name},
+			{"query", []bson.M{bson.M{"_id": _u}}}}, results)
+		c.Database.Run(bson.D{{"insert", c.Name},
+			{"documents", []bson.M{doc}}}, results)
+	}
 	return nil
 }
 
