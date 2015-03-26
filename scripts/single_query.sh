187971552e9303e112ecdb4e1b84f4979a1b50bb
Rui Zhang <rui.zhang@mongodb.com>
2015-03-26 16:13:44 -0700

    update single_query.sh, add data staging flag

--- scripts/single_query.sh
+++ scripts/single_query.sh
@@ -13,7 +13,7 @@ export HT_CMD_WORKERS=16
 export HT_CMD_MONITOR_INTERVAL=10
 
 # how long shall the run in sercond, 0 means infinity
-export HT_CMD_TOTAL_DOC=1000000
+export HT_CMD_TOTAL_DOC=100000
 
 # how long shall the run in sercond, 0 means infinity
 export HT_CMD_TOTAL_TIME=0
@@ -26,6 +26,9 @@ export HT_USE_LEGACY_WRITE=0
 
 # server URL
 export HT_SERVER_URL="localhost:27017"
+
+# make insert in staging mode
+export HT_STAGE_INSERT="yes"
 
 source config.sh
 
